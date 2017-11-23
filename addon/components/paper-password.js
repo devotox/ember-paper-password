9086f1146f5ef93f620265873c0124d8fe231421
Devonte <devo.tox.89@gmail.com>
2017-11-23 17:54:11 +0000

    Add password strength

--- addon/components/paper-password.js
+++ addon/components/paper-password.js
@@ -1,18 +1,44 @@
 import $ from 'jquery';
+
+import strength from 'password-strength';
+
+import { computed } from '@ember/object';
+
 import PaperInput from 'ember-paper/components/paper-input';
-import layout from 'ember-paper/templates/components/paper-input';
+
+import layout from '../templates/components/paper-password';
+
+// import layout from 'ember-paper/templates/components/paper-input';
 
 export default PaperInput.extend({
 	layout,
 
+	errors: [],
+
 	reveal: true,
+
+	minStrength: 3,
 
 	type: 'password',
 
-	didInsertElement() {
-		if(!this.get('reveal')) { return; }
+	customValidations: [],
 
-		if (this.isDestroyed) { return; }
+	passwordStrength: strength,
+
+	showPasswordStrength: true,
+
+	strengthLabel: 'Password Strength',
+
+	strengthLevels: ['Very Poor', 'Poor', 'Fair', 'Good', 'Excellent'],
+
+	didInsertElement() {
+		if (!this.get('reveal')) {
+			return;
+		}
+
+		if (this.isDestroyed) {
+			return;
+		}
 
 		this._super(...arguments);
 
@@ -20,14 +46,40 @@ export default PaperInput.extend({
 	},
 
 	willDestroyElement() {
-		if(!this.get('reveal')) { return; }
+		if (!this.get('reveal')) {
+			return;
+		}
 
-		if (this.isDestroyed) { return; }
+		if (this.isDestroyed) {
+			return;
+		}
 
 		this._super(...arguments);
 
 		this.unbindIcon();
 	},
+
+	strength: computed('value', function() {
+		let value = this.get('value');
+		let defaultStrength = { score: 0 };
+		let passwordStrength = this.get('passwordStrength');
+		return !value ? defaultStrength : passwordStrength(value);
+	}),
+
+	strengthValue: computed('value', function() {
+		let strength = this.get('strength');
+		return strength.score / 4 * 100;
+	}),
+
+	strengthLevel: computed('value', function() {
+		let strength = this.get('strength');
+		return this.get('strengthLevels')[strength.score];
+	}),
+
+	strengthWarning: computed('value', function() {
+		let strength = this.get('strength');
+		return strength.score < this.get('minStrength');
+	}),
 
 	bindEvent() {
 		let namespace = '.passwordReveal';
@@ -38,7 +90,9 @@ export default PaperInput.extend({
 	},
 
 	unbindIcon() {
-		if (this.isDestroyed) { return; }
+		if (this.isDestroyed) {
+			return;
+		}
 
 		let bindEvent = this.bindEvent();
 
@@ -46,7 +100,9 @@ export default PaperInput.extend({
 	},
 
 	attachIcon() {
-		if (this.isDestroyed) { return; }
+		if (this.isDestroyed) {
+			return;
+		}
 
 		let namespace = '.passwordReveal';
 		let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
@@ -70,20 +126,21 @@ export default PaperInput.extend({
 
 		let toggleEye = ($element, $icon) => {
 			let type = $element.attr('type');
-			// let types = ['password', 'text']
 			let defaultType = 'password';
 			let newType = 'text';
 
 			$element.attr('type', type === defaultType ? newType : defaultType);
 
-			return type === defaultType
-				? $icon.addClass(open).removeClass(closed)
-				: $icon.addClass(closed).removeClass(open);
+			return type === defaultType ?
+				$icon.addClass(open).removeClass(closed) :
+				$icon.addClass(closed).removeClass(open);
 		};
 
 		let $this = this.$('input')
 
-		let $icon = $('<i/>', { 'class': _class(closed) }).css(_style());
+		let $icon = $('<i/>', {
+			'class': _class(closed)
+		}).css(_style());
 
 		$icon.bind(bindEvent, (e) => {
 			e.stopImmediatePropagation();
--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'paper-password'
+	name: 'paper-password',
+	options: {
+		babel: {
+			plugins: [
+				'transform-object-rest-spread',
+				'transform-async-to-generator'
+			]
+		},
+		'ember-cli-babel': {
+			compileModules: true,
+			includePolyfill: true,
+			disableDebugTooling: true
+		}
+	}
 };
